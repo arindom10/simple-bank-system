28b55b8008d6738f717e23980a894ba4353ae1d2
arindom10 <karindom7@gmail.com>
2022-09-27 00:24:08 +0600

    done

--- js/withdraw.js
+++ js/withdraw.js
@@ -2,21 +2,38 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
    const withdrawField = document.getElementById('withdraw-field');
    const newWithdrawAmountString = withdrawField.value;
    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+   console.log(newWithdrawAmount);
+   
+   withdrawField.value = '';
+
+   if (isNaN(newWithdrawAmount)) {
+      alert('Please Provide a valid number')
+      return;
+   }
    
    const withdrawTotalElement = document.getElementById('withdraw-total');
    const previousWithdrawTotalString = withdrawTotalElement.innerText;
    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
    
-   const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-   withdrawTotalElement.innerText = currentWithdrawTotal;
+   
 
    const balanceTotalElement = document.getElementById('balance-total');
    const previousBalanceTotalString = balanceTotalElement.innerText;
    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+   
+
+   if(newWithdrawAmount > previousBalanceTotal){
+      alert('Not Enough Balance');
+      return;
+   }
+
+   const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+   withdrawTotalElement.innerText = currentWithdrawTotal;
+
    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
    balanceTotalElement.innerText = newBalanceTotal;
 
 
-   withdrawField.value = '';
+   
 })
